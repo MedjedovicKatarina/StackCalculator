99282014bbeaddbbbb6c9ca07a39670cb1326908
KatarinaMedjedovic <medjedovic.katarina@gmail.com>
2018-10-28 02:44:54 +0200

    Implemented calculator operations + Added the JavaDocs to the code

--- src/calc/StackCalculator.java
+++ src/calc/StackCalculator.java
@@ -2,20 +2,37 @@ package calc;
 
 import java.util.Stack;
 
+/**
+ * 
+ * This class handles the data of a single Calculator. It contains its stack which is used
+ * for storing the operands.
+ * 
+ * @author Katarina Mededovic
+ * 
+ */
 public class StackCalculator {
 	
 	private Stack<Integer> stack;
 	
+	/**
+	 * 
+	 * The constructor for the StackCalculator class. This constructor initializes the stack field
+	 * without any arguments being passed to it.
+	 * 
+	 */
 	public StackCalculator() {
 		this.stack = new Stack<Integer>();
 	}
 
+	/**
+	 * 
+	 * Returns the stack of the calculator in question.
+	 * 
+	 * @return Stack of the calculator in question.
+	 * 
+	 */
 	public Stack<Integer> getStack() {
 		return stack;
-	}
-	
-	public void setStack(Stack<Integer> stack) {
-		this.stack = stack;
 	}
 	
 }
